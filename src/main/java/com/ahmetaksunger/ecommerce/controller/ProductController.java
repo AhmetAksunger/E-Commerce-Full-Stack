c434e756db927d50c3d1ee403b77905a5beace7d
Ahmet <ahmetaksungerbilgi@gmail.com>
2023-08-07 02:04:02 +0300

    Adding Cross Origin annotation

--- src/main/java/com/ahmetaksunger/ecommerce/controller/ProductController.java
+++ src/main/java/com/ahmetaksunger/ecommerce/controller/ProductController.java
@@ -8,7 +8,6 @@ import com.ahmetaksunger.ecommerce.service.ProductService;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -23,6 +22,7 @@ import java.util.List;
 @RequestMapping("/api/v1/products")
 @SecurityRequirement(name = "bearerAuth")
 @RequiredArgsConstructor
+@CrossOrigin
 public class ProductController {
 
     private final ProductService productService;
