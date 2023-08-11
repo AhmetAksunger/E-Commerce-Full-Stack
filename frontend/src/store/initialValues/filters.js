9fd8cc45649ba73f3f7a1fb5664a784dc5035b99
Ahmet <ahmetaksungerbilgi@gmail.com>
2023-08-11 15:35:58 +0300

    Store & Reducer for filters

---
+++ frontend/src/store/initialValues/filters.js
@@ -0,0 +1,10 @@
+export const initialFilters = {
+    sort: undefined,
+    order: undefined,
+    searc: undefined,
+    categoryIds: undefined,
+    minPrice: undefined,
+    maxPrice: undefined,
+    page: undefined,
+    size: undefined,
+}
--- frontend/src/store/rootReducer.js
+++ frontend/src/store/rootReducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import authReducer from "./reducers/authReducer";
+import filterReducer from "./reducers/filterReducer";
 
 const rootReducer = combineReducers({
     auth : authReducer,
+    filter : filterReducer
 });
 
 export default rootReducer;
