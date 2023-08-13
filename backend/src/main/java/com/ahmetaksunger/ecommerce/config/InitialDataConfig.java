cd8421a250eee2ef13ae1304ca19cd4d7a4c8372
Ahmet <ahmetaksungerbilgi@gmail.com>
2023-08-13 03:57:46 +0300

    Seperated codes to private methods for readability

--- backend/src/main/java/com/ahmetaksunger/ecommerce/config/InitialDataConfig.java
+++ backend/src/main/java/com/ahmetaksunger/ecommerce/config/InitialDataConfig.java
@@ -25,20 +25,93 @@ public class InitialDataConfig {
     private final PaymentDetailRepository paymentDetailRepository;
     private final CategoryRepository categoryRepository;
 
+    private final Faker faker = new Faker();
+
+            /*
+                 Creating 20 default customers
+                 Each customer will have
+                    1 address
+                    1 payment detail
+                    1 cart
+                 */
     /**
-     * The default method used for creating and saving default data into the database
-     * at the beginning of the project, where instances of the {@link User} and {@link Seller}
-     * entity objects are created and persisted as default data.
+     * Creates specified amount of {@link Customer}s.
+     * Each customer has 1 {@link Address}, 1 {@link PaymentDetail} and 1 {@link Cart}
+     *
+     * @param amount amount of customers
      */
+    private void createCustomers(int amount){
+        for (int i = 0; i < amount; i++) {
+
+            final String phoneNumber = faker.phoneNumber().cellPhone()
+                    .replace("-", "")
+                    .replace(" ", "")
+                    .replace(".", "")
+                    .replace("(", "")
+                    .replace(")", "");
+
+            final Customer customer = Customer.builder()
+                    .email(faker.internet().emailAddress())
+                    .password(passwordEncoder.encode("test123"))
+                    .userType(UserType.CUSTOMER)
+                    .fullName(faker.name().fullName())
+                    .phoneNumber(phoneNumber)
+                    .createdAt(new Date())
+                    .build();
+
+            final Cart cart = Cart.builder()
+                    .createdAt(new Date())
+                    .customer(customer)
+                    .build();
+
+            customer.setCart(cart);
+            customerRepository.save(customer);
+
+            final Address address = Address.builder()
+                    .address(faker.address().fullAddress())
+                    .city(faker.address().city())
+                    .country(Country.TURKEY)
+                    .zipCode(faker.address().zipCode())
+                    .user(customer)
+                    .createdAt(new Date())
+                    .build();
+
+            addressRepository.save(address);
+
+            final PaymentDetail paymentDetail = PaymentDetail.builder()
+                    .creditCardNumber(faker.business().creditCardNumber())
+                    .cvv("123")
+                    .expirationDate(faker.business().creditCardExpiry())
+                    .user(customer)
+                    .build();
+
+            paymentDetailRepository.save(paymentDetail);
+        }
+    }
+
+    /**
+     * Creates specified amount of @{@link Category}s
+     * @param amount amount of categories
+     */
+    private void createCategories(int amount) {
+        for (int i = 0; i < 20; i++) {
+
+            final Category category = Category.builder()
+                    .name(faker.commerce().department())
+                    .description(faker.lorem().paragraph(2))
+                    .createdAt(new Date())
+                    .build();
+
+            categoryRepository.save(category);
+        }
+    }
+
     @Bean
     @Transactional
     CommandLineRunner initialData() {
         return new CommandLineRunner() {
             @Override
             public void run(String... args) throws Exception {
-
-                //Faker
-                Faker faker = new Faker();
 
                 // Creating Default Admin User
                 final User defaultAdminUserEntity = User.builder()
@@ -50,74 +123,13 @@ public class InitialDataConfig {
 
                 userRepository.save(defaultAdminUserEntity);
 
-                /*
-                  Creating 20 categories
-                 */
+                // Creating Default Customers
+                createCustomers(20);
 
-                for (int i = 0; i < 20; i++) {
+                // Creating categories
+                createCategories(20);
 
-                    final Category category = Category.builder()
-                            .name(faker.commerce().department())
-                            .description(faker.lorem().paragraph(2))
-                            .createdAt(new Date())
-                            .build();
 
-                    categoryRepository.save(category);
-                }
-
-                /*
-                 Creating 20 default customers
-                 Each customer will have
-                    1 address
-                    1 payment detail
-                    1 cart
-                 */
-                for (int i = 0; i < 20; i++) {
-
-                    final String phoneNumber = faker.phoneNumber().cellPhone()
-                            .replace("-", "")
-                            .replace(" ", "")
-                            .replace(".", "")
-                            .replace("(", "")
-                            .replace(")", "");
-
-                    final Customer customer = Customer.builder()
-                            .email(faker.internet().emailAddress())
-                            .password(passwordEncoder.encode("test123"))
-                            .userType(UserType.CUSTOMER)
-                            .fullName(faker.name().fullName())
-                            .phoneNumber(phoneNumber)
-                            .createdAt(new Date())
-                            .build();
-
-                    final Cart cart = Cart.builder()
-                            .createdAt(new Date())
-                            .customer(customer)
-                            .build();
-
-                    customer.setCart(cart);
-                    customerRepository.save(customer);
-
-                    final Address address = Address.builder()
-                            .address(faker.address().fullAddress())
-                            .city(faker.address().city())
-                            .country(Country.TURKEY)
-                            .zipCode(faker.address().zipCode())
-                            .user(customer)
-                            .createdAt(new Date())
-                            .build();
-
-                    addressRepository.save(address);
-
-                    final PaymentDetail paymentDetail = PaymentDetail.builder()
-                            .creditCardNumber(faker.business().creditCardNumber())
-                            .cvv("123")
-                            .expirationDate(faker.business().creditCardExpiry())
-                            .user(customer)
-                            .build();
-
-                    paymentDetailRepository.save(paymentDetail);
-                }
             }
         };
     }
