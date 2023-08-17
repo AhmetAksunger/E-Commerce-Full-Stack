ef7aefe6ecf43bb6fd7581023f29dec846724c77
Ahmet <ahmetaksungerbilgi@gmail.com>
2023-08-17 21:33:12 +0300

    adding the auth state to the local storage

--- frontend/src/store/actions/authActions.js
+++ frontend/src/store/actions/authActions.js
@@ -2,6 +2,7 @@ export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
 export const LOGOUT_SUCCESS = "LOGOUT_SUCCESS";
 
 export function loginSuccess(authState){
+    localStorage.setItem('auth',JSON.stringify({...authState, isLoggedIn:true}));
     return {
         type:LOGIN_SUCCESS,
         payload:authState
@@ -9,6 +10,7 @@ export function loginSuccess(authState){
 }
 
 export function logoutSuccess(){
+    localStorage.removeItem('auth');
     return {
         type:LOGOUT_SUCCESS
     }
