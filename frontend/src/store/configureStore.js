65b24fba64965e71ba17dfb26ddb5ff0c0179eec
Ahmet <ahmetaksungerbilgi@gmail.com>
2023-08-17 21:33:01 +0300

    starting the redux store with the locally stored data

--- frontend/src/store/configureStore.js
+++ frontend/src/store/configureStore.js
@@ -1,7 +1,18 @@
 import { legacy_createStore as createStore } from 'redux';
 import rootReducer from './rootReducer';
 import { devToolsEnhancer } from 'redux-devtools-extension';
+import { defaultAuthState } from "./initialValues/authState";
+
 
 export function configureStore(){
-    return createStore(rootReducer,devToolsEnhancer())
+
+    const authState = localStorage.getItem('auth');
+
+    let authStateInLocalStorage = defaultAuthState;
+
+    if(authState){
+        authStateInLocalStorage = JSON.parse(authState);
+    }
+
+    return createStore(rootReducer,{auth:authStateInLocalStorage},devToolsEnhancer())
 }
