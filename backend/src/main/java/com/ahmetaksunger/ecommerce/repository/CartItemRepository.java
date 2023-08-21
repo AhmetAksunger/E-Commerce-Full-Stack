3d7f44792c8bc1a0aea4df6db99f7ee08b698e39
Ahmet <ahmetaksungerbilgi@gmail.com>
2023-08-22 00:30:12 +0300

    find by cart id and product id method

--- backend/src/main/java/com/ahmetaksunger/ecommerce/repository/CartItemRepository.java
+++ backend/src/main/java/com/ahmetaksunger/ecommerce/repository/CartItemRepository.java
@@ -3,6 +3,10 @@ package com.ahmetaksunger.ecommerce.repository;
 import com.ahmetaksunger.ecommerce.model.CartItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CartItemRepository extends JpaRepository<CartItem,Long> {
-    void deleteAllByCartId(long cartId);
+    void deleteAllByCartId(Long cartId);
+
+    Optional<CartItem> findByCartIdAndProductId(Long cartId,Long productId);
 }
