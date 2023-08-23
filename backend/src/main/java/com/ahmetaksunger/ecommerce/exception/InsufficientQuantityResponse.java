acd7ab93aef93155994b99997e8707fff488f4ac
Ahmet <ahmetaksungerbilgi@gmail.com>
2023-08-24 02:28:50 +0300

    fixed the bug #47

--- backend/src/main/java/com/ahmetaksunger/ecommerce/exception/InsufficientQuantityResponse.java
+++ backend/src/main/java/com/ahmetaksunger/ecommerce/exception/InsufficientQuantityResponse.java
@@ -3,6 +3,7 @@ package com.ahmetaksunger.ecommerce.exception;
 import com.ahmetaksunger.ecommerce.dto.response.ProductVM;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.experimental.SuperBuilder;
 
@@ -10,6 +11,7 @@ import java.util.List;
 
 @Getter @Setter
 @SuperBuilder
+@NoArgsConstructor
 public class InsufficientQuantityResponse extends DefaultExceptionResponse{
 
     List<ProductVM> productsWithInsufficientStock;
