8db618d4c29b594660a8d98f3b78968cd90b784f
Ahmet <ahmetaksungerbilgi@gmail.com>
2023-08-25 18:20:17 +0300

    Product Order Info to ProductVM converter

--- backend/src/main/java/com/ahmetaksunger/ecommerce/dto/converter/ProductVMConverter.java
+++ backend/src/main/java/com/ahmetaksunger/ecommerce/dto/converter/ProductVMConverter.java
@@ -1,6 +1,7 @@
 package com.ahmetaksunger.ecommerce.dto.converter;
 
 import com.ahmetaksunger.ecommerce.dto.response.CategoryVM;
+import com.ahmetaksunger.ecommerce.dto.response.ProductOrderInfo;
 import com.ahmetaksunger.ecommerce.dto.response.ProductVM;
 import com.ahmetaksunger.ecommerce.dto.response.SellerVM;
 import com.ahmetaksunger.ecommerce.mapper.MapperService;
@@ -36,5 +37,22 @@ public class ProductVMConverter {
                 .categories(from.getCategories().stream().map(category -> mapperService.forResponse().map(category, CategoryVM.class)).collect(Collectors.toList()))
                 .build();
 
+    }
+
+    public ProductVM convert(ProductOrderInfo from){
+
+        return ProductVM.builder()
+                .id(from.getProduct().getId())
+                .name(from.getProduct().getName())
+                .description(from.getProduct().getDescription())
+                .price(from.getProduct().getPrice())
+                .quantity(from.getProduct().getQuantity())
+                .orderCount(from.getOrderCount())
+                .logo(from.getProduct().getLogo())
+                .createdAt(from.getProduct().getCreatedAt())
+                .updatedAt(from.getProduct().getUpdatedAt())
+                .seller(mapperService.forResponse().map(from.getProduct().getSeller(), SellerVM.class))
+                .categories(from.getProduct().getCategories().stream().map(category -> mapperService.forResponse().map(category, CategoryVM.class)).collect(Collectors.toList()))
+                .build();
     }
 }
