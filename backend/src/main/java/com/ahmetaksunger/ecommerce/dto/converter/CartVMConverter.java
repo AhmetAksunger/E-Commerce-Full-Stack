47bea65530dc6626e0c08945eb97b9593f1d5db1
Ahmet <ahmetaksungerbilgi@gmail.com>
2023-08-25 15:55:43 +0300

    CartVMConverter

---
+++ backend/src/main/java/com/ahmetaksunger/ecommerce/dto/converter/CartVMConverter.java
@@ -0,0 +1,30 @@
+package com.ahmetaksunger.ecommerce.dto.converter;
+
+import com.ahmetaksunger.ecommerce.dto.response.CartVM;
+import com.ahmetaksunger.ecommerce.dto.response.CustomerVM;
+import com.ahmetaksunger.ecommerce.mapper.MapperService;
+import com.ahmetaksunger.ecommerce.model.Cart;
+import com.ahmetaksunger.ecommerce.service.CartService;
+import com.ahmetaksunger.ecommerce.service.PriceCalculator;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.stream.Collectors;
+
+@Component
+@RequiredArgsConstructor
+public class CartVMConverter {
+
+    private final MapperService mapperService;
+    private final CartService cartService;
+    private final CartItemVMConverter cartItemVMConverter;
+    public CartVM convert(Cart from){
+        return CartVM.builder()
+                .id(from.getId())
+                .customer(mapperService.forResponse().map(from.getCustomer(), CustomerVM.class))
+                .cartItems(from.getCartItems().stream().map(cartItemVMConverter::convert).collect(Collectors.toList()))
+                .totalProductCount(cartService.calculateTotalProductCount(from))
+                .total(PriceCalculator.calculateTotal(from))
+                .build();
+    }
+}
