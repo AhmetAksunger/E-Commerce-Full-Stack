d933fb8c701c41518479fa423e973c162ade5a4d
AhmetAksunger <ahmetaksungerbilgi@gmail.com>
2023-09-03 23:37:47 +0300

    Create ProductListRequest

---
+++ backend/src/main/java/com/ahmetaksunger/ecommerce/dto/request/product/ProductListRequest.java
@@ -0,0 +1,52 @@
+package com.ahmetaksunger.ecommerce.dto.request.product;
+
+import com.ahmetaksunger.ecommerce.model.Product;
+import com.ahmetaksunger.ecommerce.spesification.ProductSpecification;
+import com.ahmetaksunger.ecommerce.util.ECommerceFiltering;
+import com.ahmetaksunger.ecommerce.util.ECommercePagingRequest;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.jpa.domain.Specification;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Getter
+@Setter
+public class ProductListRequest extends ECommercePagingRequest implements ECommerceFiltering<Product> {
+
+    @NotNull
+    private Filter filter;
+
+    @Getter
+    @Setter
+    public static class Filter {
+        private String search;
+        private List<Long> categoryIds;
+        private BigDecimal minPrice;
+        private BigDecimal maxPrice;
+    }
+
+    @Override
+    public Specification<Product> toSpecification() {
+
+        Specification<Product> specification = Specification.where(null);
+
+        if (filter.getSearch() != null && !filter.getSearch().isEmpty()) {
+            specification = specification.and(ProductSpecification.searchByKeyword(filter.getSearch()));
+        }
+
+        if (filter.getCategoryIds() != null && !filter.getCategoryIds().isEmpty()) {
+            specification = specification.and(ProductSpecification.withCategoryIds(filter.getCategoryIds()));
+        }
+        if (filter.getMinPrice() != null) {
+            specification = specification.and(ProductSpecification.withPriceGreaterThanOrEqualTo(filter.getMinPrice()));
+        }
+        if (filter.getMinPrice() != null) {
+            specification = specification.and(ProductSpecification.withPriceLessThanOrEqualTo(filter.getMinPrice()));
+        }
+
+        return specification;
+    }
+}
