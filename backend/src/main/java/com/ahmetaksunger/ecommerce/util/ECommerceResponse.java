7ff5609422a8c0bf8f6f3295983b9affaad9c2c3
AhmetAksunger <ahmetaksungerbilgi@gmail.com>
2023-09-09 19:43:34 +0300

    Create ECommerceResponse

---
+++ backend/src/main/java/com/ahmetaksunger/ecommerce/util/ECommerceResponse.java
@@ -0,0 +1,53 @@
+package com.ahmetaksunger.ecommerce.util;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NonNull;
+import org.springframework.http.HttpStatus;
+
+@Builder
+@AllArgsConstructor
+@Getter
+public class ECommerceResponse<T> {
+
+    public ECommerceResponse(@NonNull T response, @NonNull HttpStatus httpStatus) {
+        this.response = response;
+        this.httpStatus = httpStatus;
+        this.isSuccess = httpStatus.is2xxSuccessful();
+    }
+
+    private T response;
+
+    private HttpStatus httpStatus;
+
+    @Builder.Default
+    private Long time = System.currentTimeMillis();
+
+    private Boolean isSuccess;
+
+    public static <T> ECommerceResponse<T> successOf(T response) {
+        return ECommerceResponse.<T>builder()
+                .response(response)
+                .isSuccess(true)
+                .httpStatus(HttpStatus.OK)
+                .build();
+    }
+
+    public static <T> ECommerceResponse<T> createdOf(T response) {
+        return ECommerceResponse.<T>builder()
+                .response(response)
+                .isSuccess(true)
+                .httpStatus(HttpStatus.CREATED)
+                .build();
+    }
+
+    public static <T> ECommerceResponse<T> badRequestOf(T response) {
+        return ECommerceResponse.<T>builder()
+                .response(response)
+                .isSuccess(false)
+                .httpStatus(HttpStatus.BAD_REQUEST)
+                .build();
+    }
+
+}
